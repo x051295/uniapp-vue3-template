b53c3e83530576737a1ab3c32b642462c3f62fb8
mr_aothoy <60827319+x051295@users.noreply.github.com>
2024-09-30 18:01:05 +0800

    feat: fix

---
+++ pages/i18.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { locale, setLocale } = useI18n()
+</script>
+
+<template>
+  <div class="h-full">
+    <div>
+      <button @click="setLocale('en')">英文</button>
+      <br />
+      <button @click="setLocale('zh-CN')">中文</button>
+      <p>{{ locale }}</p>
+      <h1>{{ $t('welcome') }}</h1>
+    </div>
+  </div>
+</template>
